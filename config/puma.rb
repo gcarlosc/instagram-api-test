d1aa9a4d63013e94b0f5015bab977a29dd5a17e6
Giancarlo Solís <gsoliscastillo@gmail.com>
2023-11-13 18:32:47 -0500

    change

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@ environment ENV.fetch('RACK_ENV') { 'development' }
 #workers Integer(ENV.fetch('WEB_CONCURRENCY') { 5 })
 if ENV['RACK_ENV'] == 'production'
   bind "unix:///var/run/puma/my_app.sock"
-  activate_control_app "unix:///var/run/puma/my_app_control.sock", {no_token: true}
+  # activate_control_app "unix:///var/run/puma/my_app_control.sock", {no_token: true}
 else
   bind "tcp://127.0.0.1"
 end
